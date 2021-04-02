103ca56c85f13af8c5f22a5aeb1d3ca7b2957a92
ImSeaWorld <ImSeaWorld@users.noreply.github.com>
2021-04-02 02:08:27 -0700

    Big. Dick. Henry...

---
+++ src/components/marketplace/powerList.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        big dick henry
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'powerList',
+    props: {
+        coinprices: Object,
+    },
+};
+</script>
+
+<style>
+</style>
