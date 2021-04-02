a31a951e19cadc72325fea4ee24d4105134ebfcc
ImSeaWorld <ImSeaWorld@users.noreply.github.com>
2021-04-02 02:12:50 -0700

    Facepalm...

--- src/main.js
+++ src/main.js
@@ -6,36 +6,6 @@ import store from './store';
 import './quasar';
 import moment from 'moment';
 import punycode from 'punycode';
-/*import { app, dialog, autoUpdater } from 'electron';
-
-const server = 'https://your-deployment-url.com';
-const url = `${server}/update/${process.platform}/${app.getVersion()}`;
-autoUpdater.setFeedURL({ url });
-autoUpdater.checkForUpdates();
-
-setInterval(() => {
-    autoUpdater.checkForUpdates();
-}, 60000);
-
-autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
-    const dialogOpts = {
-        type: 'info',
-        buttons: ['Restart', 'Later'],
-        title: 'Application Update',
-        message: process.platform === 'win32' ? releaseNotes : releaseName,
-        detail:
-            'A new version has been downloaded. Restart the application to apply the updates.',
-    };
-
-    dialog.showMessageBox(dialogOpts).then((returnValue) => {
-        if (returnValue.response === 0) autoUpdater.quitAndInstall();
-    });
-});
-
-autoUpdater.on('error', (message) => {
-    console.error('There was a problem updating the application');
-    console.error(message);
-});*/
 
 Vue.config.productionTip = false;
 
