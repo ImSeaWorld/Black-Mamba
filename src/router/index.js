2e99385420157791f37bb7894c8ae83974999cb1
ImSeaWorld <ImSeaWorld@users.noreply.github.com>
2021-04-05 23:17:35 -0700

    Removed redundant routes

--- src/router/index.js
+++ src/router/index.js
@@ -20,11 +20,6 @@ const routes = [
         name: 'Coin',
         component: () => import('../views/Coin.vue'),
     },
-    /*{
-        path: '/boyses',
-        name: 'Boyses',
-        component: () => import('../views/boyses.vue'),
-    },*/
     {
         path: '/spa',
         name: 'SPABuilder',
@@ -39,15 +34,6 @@ const routes = [
         path: '/marketplace',
         name: 'Marketplace',
         component: () => import('../views/Marketplace.vue'),
-    },
-    {
-        path: '/about',
-        name: 'About',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () =>
-            import(/* webpackChunkName: "about" */ '../views/About.vue'),
     },
 ];
 
